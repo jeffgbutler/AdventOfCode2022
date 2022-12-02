17a0a8aee1f47a35385b91eb6912e67b88760635
Jeff Butler <jeffgbutler@gmail.com>
2022-12-02 16:12:43 -0500

    Day 1 Solution

---
+++ Day01/day01.go
@@ -0,0 +1,96 @@
+package Day01
+
+import (
+	"bufio"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func readInputFile(f string) ([]string, error) {
+	var lines []string
+	file, err := os.Open(f)
+	if err != nil {
+		return lines, err
+	}
+	defer file.Close()
+
+	bufScanner := bufio.NewScanner(file)
+
+	for bufScanner.Scan() {
+		lines = append(lines, bufScanner.Text())
+	}
+
+	return lines, bufScanner.Err()
+}
+
+type elf struct {
+	elfNumber int
+	calories  int
+}
+
+func convertToElves(lines []string) ([]elf, error) {
+	var allElves []elf
+	var currentElf elf
+
+	for _, line := range lines {
+		if len(line) == 0 {
+			if currentElf.calories > 0 {
+				allElves = append(allElves, currentElf)
+				currentElf = elf{len(allElves) + 1, 0}
+			} else {
+				continue
+			}
+		} else {
+			num, err := strconv.Atoi(line)
+			if err != nil {
+				return allElves, err
+			}
+			currentElf.calories += num
+		}
+	}
+
+	if currentElf.calories > 0 {
+		allElves = append(allElves, currentElf)
+	}
+	return allElves, nil
+}
+
+func findGreatestElf(elves []elf) elf {
+	sortElvesByCaloriesMostToLeast(elves)
+	return elves[0]
+}
+
+func findTopThreeElves(elves []elf) []elf {
+	sortElvesByCaloriesMostToLeast(elves)
+	return elves[:3]
+}
+
+func sortElvesByCaloriesMostToLeast(elves []elf) {
+	sort.Slice(elves, func(i, j int) bool {
+		return elves[i].calories > elves[j].calories
+	})
+}
+
+func compareElfSlices(a, b []elf) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i, e := range a {
+		if e != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func calculateTotalCalories(elves []elf) int {
+	total := 0
+	for _, e := range elves {
+		total += e.calories
+	}
+
+	return total
+}
---
+++ Day01/day01_test.go
@@ -0,0 +1,127 @@
+package Day01
+
+import "testing"
+
+func Test_readInputFile(t *testing.T) {
+	lines, err := readInputFile("testdata/example.txt")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(lines) != 20 {
+		t.Error("Expected 20 lines, got", len(lines))
+	}
+}
+
+func Test_Converter(t *testing.T) {
+	lines, err := readInputFile("testdata/example.txt")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	elves, err := convertToElves(lines)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(elves) != 5 {
+		t.Error("Expected 5 elves, got", len(elves))
+	}
+}
+
+func Test_findGreatestElfWithExampleData(t *testing.T) {
+	lines, err := readInputFile("testdata/example.txt")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	elves, err := convertToElves(lines)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	greatestElf := findGreatestElf(elves)
+	expectedElf := elf{4, 24000}
+	if greatestElf != expectedElf {
+		t.Error("Expected elf", expectedElf, "got elf", greatestElf)
+	}
+}
+
+func Test_findGreatestElfWithActualData(t *testing.T) {
+	lines, err := readInputFile("testdata/mydata.txt")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	elves, err := convertToElves(lines)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	greatestElf := findGreatestElf(elves)
+	expectedElf := elf{172, 70509}
+	if greatestElf != expectedElf {
+		t.Error("Expected elf", expectedElf, "got elf", greatestElf)
+	}
+}
+
+func Test_findTopThreeElvesWithExampleData(t *testing.T) {
+	lines, err := readInputFile("testdata/example.txt")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	elves, err := convertToElves(lines)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	topThreeElves := findTopThreeElves(elves)
+	expectedTopThree := []elf{{4, 24000}, {3, 11000}, {5, 10000}}
+
+	if !compareElfSlices(topThreeElves, expectedTopThree) {
+		t.Error("Expected top three elves", expectedTopThree, "got", topThreeElves)
+	}
+
+	totalCalories := calculateTotalCalories(topThreeElves)
+	expectedCalories := 45000
+	if totalCalories != expectedCalories {
+		t.Error("Expected top three elves to have", expectedCalories, "calories, got", totalCalories)
+	}
+}
+
+func Test_findTopThreeElvesWithActualData(t *testing.T) {
+	lines, err := readInputFile("testdata/mydata.txt")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	elves, err := convertToElves(lines)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	topThreeElves := findTopThreeElves(elves)
+	expectedTopThree := []elf{{172, 70509}, {80, 69894}, {28, 68164}}
+
+	if !compareElfSlices(topThreeElves, expectedTopThree) {
+		t.Error("Expected top three elves", expectedTopThree, "got", topThreeElves)
+	}
+
+	totalCalories := calculateTotalCalories(topThreeElves)
+	expectedCalories := 208567
+	if totalCalories != expectedCalories {
+		t.Error("Expected top three elves to have", expectedCalories, "calories, got", totalCalories)
+	}
+}
