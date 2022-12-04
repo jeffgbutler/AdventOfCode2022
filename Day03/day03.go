23c0da31701eb3cf4e8b396ee24e9d8c8189a07d
Jeff Butler <jeffgbutler@gmail.com>
2022-12-04 16:37:27 -0500

    Day 3, Part 1

--- Day03/day03.go
+++ Day03/day03.go
@@ -1,5 +1,7 @@
 package Day03
 
+import "strings"
+
 type rucksackContents struct {
 	compartment1 string
 	compartment2 string
@@ -8,9 +10,44 @@ type rucksackContents struct {
 func convertToRucksacks(inputLines []string) []rucksackContents {
 	var rucksacks []rucksackContents
 
+	for _, inputLine := range inputLines {
+		length := len(inputLine)
+		mid := length / 2
+
+		rucksack := rucksackContents{inputLine[:mid], inputLine[mid:length]}
+		rucksacks = append(rucksacks, rucksack)
+	}
+
 	return rucksacks
 }
 
-func calculateValue(r rune) int {
-	return 1
+func calculateRucksackTotal(rucksacks []rucksackContents) int {
+	value := 0
+
+	for _, rucksack := range rucksacks {
+		value += calculateRucksack(rucksack)
+	}
+
+	return value
+}
+
+func calculateRucksack(rucksack rucksackContents) int {
+	for _, r := range rucksack.compartment1 {
+		if strings.ContainsRune(rucksack.compartment2, r) {
+			return calculateLetterValue(r)
+		}
+	}
+
+	return -1
+}
+
+func calculateLetterValue(r rune) int {
+	val := int(r)
+	if val > 90 {
+		//lower case
+		return val - int('a') + 1
+	} else {
+		// upper case
+		return val - int('A') + 27
+	}
 }
--- Day03/day03_test.go
+++ Day03/day03_test.go
@@ -7,34 +7,52 @@ import (
 
 func Test_Part1ExampleData(t *testing.T) {
 	inputLines, _ := inputfile.Read("testdata/example.txt")
+	rucksacks := convertToRucksacks(inputLines)
+	total := calculateRucksackTotal(rucksacks)
 
-	if len(inputLines) != 6 {
-		t.Error("Expected 6 input lines, got", len(inputLines))
+	if total != 157 {
+		t.Error("Expected 157, got", total)
 	}
 }
 
-func Test_WhatIsARune(t *testing.T) {
-	s := "Fred"
-	s1 := s[0]
+func Test_Part1ActualData(t *testing.T) {
+	inputLines, _ := inputfile.Read("testdata/actual.txt")
+	rucksacks := convertToRucksacks(inputLines)
+	total := calculateRucksackTotal(rucksacks)
 
-	inta := int(s1)  // 97
-	intz := int('z') // 122
-	intA := int('A') // 65
-	intZ := int('Z') // 90
-
-	if inta != 22 {
-		t.Error("inta is", inta)
+	if total != 8085 {
+		t.Error("Expected 8085, got", total)
 	}
+}
 
-	if intz != 22 {
-		t.Error("intz is", intz)
+func Test_CalculateValue_a(t *testing.T) {
+	value := calculateLetterValue('a')
+
+	if value != 1 {
+		t.Error("Expected value of a to be 1, got", value)
 	}
+}
 
-	if intA != 22 {
-		t.Error("intA is", intA)
+func Test_CalculateValue_z(t *testing.T) {
+	value := calculateLetterValue('z')
+
+	if value != 26 {
+		t.Error("Expected value of z to be 26, got", value)
 	}
+}
 
-	if intZ != 22 {
-		t.Error("intZ is", intZ)
+func Test_CalculateValue_A(t *testing.T) {
+	value := calculateLetterValue('A')
+
+	if value != 27 {
+		t.Error("Expected value of A to be 27, got", value)
+	}
+}
+
+func Test_CalculateValue_Z(t *testing.T) {
+	value := calculateLetterValue('Z')
+
+	if value != 52 {
+		t.Error("Expected value of Z to be 52, got", value)
 	}
 }
