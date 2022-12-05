b38241d954294a2f4eb0e40aafb68956475070ec
Jeff Butler <jeffgbutler@gmail.com>
2022-12-04 21:24:04 -0500

    Day 4

---
+++ Day04/day04.go
@@ -0,0 +1,67 @@
+package Day04
+
+import (
+	"strconv"
+	"strings"
+)
+
+type singleRange struct {
+	start, end int
+}
+
+type doubleRange struct {
+	range1, range2 singleRange
+}
+
+func part1(inputLines []string) int {
+	total := 0
+	for _, v := range inputLines {
+		ranges := convertToDoubleRange(v)
+		if eitherRangeContained(ranges) {
+			total++
+		}
+	}
+
+	return total
+}
+
+func part2(inputLines []string) int {
+	total := 0
+	for _, v := range inputLines {
+		ranges := convertToDoubleRange(v)
+		if eitherRangeOverlaps(ranges) {
+			total++
+		}
+	}
+
+	return total
+}
+
+func convertToDoubleRange(rangesSpec string) doubleRange {
+	f := strings.Split(rangesSpec, ",")
+	return doubleRange{convertToSingleRange(f[0]), convertToSingleRange(f[1])}
+}
+
+func convertToSingleRange(rangeSpec string) singleRange {
+	f := strings.Split(rangeSpec, "-")
+	i1, _ := strconv.Atoi(f[0])
+	i2, _ := strconv.Atoi(f[1])
+	return singleRange{i1, i2}
+}
+
+func eitherRangeContained(ranges doubleRange) bool {
+	return rangeContained(ranges.range1, ranges.range2) || rangeContained(ranges.range2, ranges.range1)
+}
+
+func rangeContained(range1, range2 singleRange) bool {
+	return range1.start >= range2.start && range1.end <= range2.end
+}
+
+func eitherRangeOverlaps(ranges doubleRange) bool {
+	return rangeOverlaps(ranges.range1, ranges.range2) || rangeContained(ranges.range2, ranges.range1)
+}
+
+func rangeOverlaps(range1, range2 singleRange) bool {
+	return (range1.start >= range2.start && range1.start <= range2.end) ||
+		(range1.end >= range2.start && range1.end <= range2.end)
+}
---
+++ Day04/day04_test.go
@@ -0,0 +1,42 @@
+package Day04
+
+import (
+	"AdventOfCode2022/inputfile"
+	"testing"
+)
+
+func Test_Part1WihExampleData(t *testing.T) {
+	inputLines, _ := inputfile.Read("testdata/example.txt")
+	answer := part1(inputLines)
+
+	if answer != 2 {
+		t.Error("Expected 2, got", answer)
+	}
+}
+
+func Test_Part1WihActualData(t *testing.T) {
+	inputLines, _ := inputfile.Read("testdata/actual.txt")
+	answer := part1(inputLines)
+
+	if answer != 528 {
+		t.Error("Expected 528, got", answer)
+	}
+}
+
+func Test_Part2WihExampleData(t *testing.T) {
+	inputLines, _ := inputfile.Read("testdata/example.txt")
+	answer := part2(inputLines)
+
+	if answer != 4 {
+		t.Error("Expected 4, got", answer)
+	}
+}
+
+func Test_Part2WihActualData(t *testing.T) {
+	inputLines, _ := inputfile.Read("testdata/actual.txt")
+	answer := part2(inputLines)
+
+	if answer != 881 {
+		t.Error("Expected 881, got", answer)
+	}
+}
