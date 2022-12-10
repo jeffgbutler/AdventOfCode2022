920bc5af24679ce8a2fd78a0f6831f23e4d89297
Jeff Butler <jeffgbutler@gmail.com>
2022-12-10 13:51:50 -0500

    Day 8 - Better solution with fewer pointers

--- Day08/day08.go
+++ Day08/day08.go
@@ -2,47 +2,31 @@ package Day08
 
 import "strconv"
 
-type tree struct {
-	height  int
-	visible bool
+type treeGrid [][]int
+
+func (t treeGrid) GetHeight(row, column int) int {
+	return t[row][column]
 }
 
-type treeGrid [][]tree
-
-func (t *treeGrid) SetVisible(row, column int) {
-	(*t)[row][column].visible = true
+func (t treeGrid) Rows() int {
+	return len(t)
 }
 
-func (t *treeGrid) IsVisible(row, column int) bool {
-	return (*t)[row][column].visible
-}
-
-func (t *treeGrid) GetHeight(row, column int) int {
-	return (*t)[row][column].height
-}
-
-func (t *treeGrid) Rows() int {
-	return len(*t)
-}
-
-func (t *treeGrid) Columns() int {
-	return len((*t)[0])
+func (t treeGrid) Columns() int {
+	return len(t[0])
 }
 
 func part1(inputLines []string) int {
 
 	tg := parse(inputLines)
 
-	calc1(&tg)
-	calc2(&tg)
-	calc3(&tg)
-	calc4(&tg)
-
 	total := 0
 	for row := 0; row < tg.Rows(); row++ {
 		for column := 0; column < tg.Columns(); column++ {
-			if tg.IsVisible(row, column) {
+			myHeight := tg.GetHeight(row, column)
+			if visible(tg, myHeight, row, column) {
 				total++
+				continue
 			}
 		}
 	}
@@ -55,10 +39,10 @@ func parse(inputLines []string) treeGrid {
 	var answer treeGrid
 
 	for _, v := range inputLines {
-		var row []tree
+		var row []int
 		for j := 0; j < len(v); j++ {
 			height, _ := strconv.Atoi(string(v[j]))
-			row = append(row, tree{height: height})
+			row = append(row, height)
 		}
 		answer = append(answer, row)
 	}
@@ -66,54 +50,65 @@ func parse(inputLines []string) treeGrid {
 	return answer
 }
 
-func calc1(tg *treeGrid) {
-	for row := 0; row < tg.Rows(); row++ {
-		height := tg.GetHeight(row, 0)
-		tg.SetVisible(row, 0)
-		for column := 1; column < tg.Columns()-1; column++ {
-			if tg.GetHeight(row, column) > height {
-				height = tg.GetHeight(row, column)
-				tg.SetVisible(row, column)
-			}
-		}
-	}
+func visible(tg treeGrid, myHeight, row, column int) bool {
+	return visibleFromLeft(tg, myHeight, row, column) ||
+		visibleFromRight(tg, myHeight, row, column) ||
+		visibleFromAbove(tg, myHeight, row, column) ||
+		visibleFromBelow(tg, myHeight, row, column)
 }
 
-func calc2(tg *treeGrid) {
-	for row := 0; row < tg.Rows(); row++ {
-		height := tg.GetHeight(row, tg.Columns()-1)
-		tg.SetVisible(row, tg.Columns()-1)
-		for column := tg.Columns() - 2; column >= 1; column-- {
-			if tg.GetHeight(row, column) > height {
-				height = tg.GetHeight(row, column)
-				tg.SetVisible(row, column)
-			}
+func visibleFromLeft(tg treeGrid, myHeight, row, column int) bool {
+	if column == 0 {
+		return true
+	}
+
+	for i := column - 1; i >= 0; i-- {
+		if tg.GetHeight(row, i) >= myHeight {
+			return false
 		}
 	}
+
+	return true
 }
 
-func calc3(tg *treeGrid) {
-	for column := 0; column < tg.Columns(); column++ {
-		height := tg.GetHeight(0, column)
-		tg.SetVisible(0, column)
-		for row := 1; row < tg.Rows()-1; row++ {
-			if tg.GetHeight(row, column) > height {
-				height = tg.GetHeight(row, column)
-				tg.SetVisible(row, column)
-			}
+func visibleFromRight(tg treeGrid, myHeight, row, column int) bool {
+	if column == tg.Columns()-1 {
+		return true
+	}
+
+	for i := column + 1; i < tg.Columns(); i++ {
+		if tg.GetHeight(row, i) >= myHeight {
+			return false
 		}
 	}
+
+	return true
 }
 
-func calc4(tg *treeGrid) {
-	for column := 0; column < tg.Columns(); column++ {
-		height := tg.GetHeight(tg.Rows()-1, column)
-		tg.SetVisible(tg.Rows()-1, column)
-		for row := tg.Rows() - 2; row >= 1; row-- {
-			if tg.GetHeight(row, column) > height {
-				height = tg.GetHeight(row, column)
-				tg.SetVisible(row, column)
-			}
+func visibleFromAbove(tg treeGrid, myHeight, row, column int) bool {
+	if row == 0 {
+		return true
+	}
+
+	for i := row - 1; i >= 0; i-- {
+		if tg.GetHeight(i, column) >= myHeight {
+			return false
 		}
 	}
+
+	return true
+}
+
+func visibleFromBelow(tg treeGrid, myHeight, row, column int) bool {
+	if row == tg.Rows()-1 {
+		return true
+	}
+
+	for i := row + 1; i < tg.Rows(); i++ {
+		if tg.GetHeight(i, column) >= myHeight {
+			return false
+		}
+	}
+
+	return true
 }
