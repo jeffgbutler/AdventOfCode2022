ea2f28fcae77b29352721b5fabbe54f77267bff7
Jeff Butler <jeffgbutler@gmail.com>
2022-12-12 17:16:54 -0500

    Day 10 - Part 2

--- Day10/day10.go
+++ Day10/day10.go
@@ -1,41 +1,84 @@
 package Day10
 
 import (
-	"github.com/jeffgbutler/AdventOfCode2022/functions"
 	"strconv"
 	"strings"
 )
 
-type snapshot struct {
-	cycle, register int
-}
-
 func part1(inputLines []string) int {
 	register := 1
-	tickCount := 0
-	var snapshots []snapshot
+	cycle := 1
+	answer := 0
 
 	for _, v := range inputLines {
 		switch {
 		case v == "noop":
-			takeSnapshot(tickCount, register, &snapshots)
-			tickCount++
+			answer += calculateSignalStrength(cycle, register)
+			cycle++
 		default:
+			answer += calculateSignalStrength(cycle, register)
+			cycle++
+			answer += calculateSignalStrength(cycle, register)
+			cycle++
+
 			fields := strings.Fields(v)
 			addX, _ := strconv.Atoi(fields[1])
-			takeSnapshot(tickCount, register, &snapshots)
-			tickCount++
-			takeSnapshot(tickCount, register, &snapshots)
-			tickCount++
 			register += addX
 		}
 	}
-	return functions.Reduce(snapshots, 0, func(i int, ss snapshot) int { return i + (ss.cycle * ss.register) })
+	return answer
+
 }
 
-func takeSnapshot(beginningTickCount, register int, snapshots *[]snapshot) {
-	cycle := beginningTickCount + 1
+func calculateSignalStrength(cycle, register int) int {
 	if cycle == 20 || (cycle-20)%40 == 0 {
-		*snapshots = append(*snapshots, snapshot{cycle, register})
+		return cycle * register
+	} else {
+		return 0
 	}
+}
+
+func part2(inputLines []string) []string {
+	register := 1
+	cycle := 1
+	var answer []string
+
+	for _, v := range inputLines {
+		switch {
+		case v == "noop":
+			answer = append(answer, calculatePixel(cycle, register))
+			cycle++
+		default:
+			answer = append(answer, calculatePixel(cycle, register))
+			cycle++
+
+			answer = append(answer, calculatePixel(cycle, register))
+			cycle++
+
+			fields := strings.Fields(v)
+			addX, _ := strconv.Atoi(fields[1])
+			register += addX
+		}
+	}
+
+	s1 := strings.Join(answer[0:40], "")
+	s2 := strings.Join(answer[40:80], "")
+	s3 := strings.Join(answer[80:120], "")
+	s4 := strings.Join(answer[120:160], "")
+	s5 := strings.Join(answer[160:200], "")
+	s6 := strings.Join(answer[200:240], "")
+
+	return []string{s1, s2, s3, s4, s5, s6}
+}
+
+func calculatePixel(cycle, register int) string {
+	var answer string
+	cycle = (cycle - 1) % 40
+	if cycle >= register-1 && cycle <= register+1 {
+		answer = "#"
+	} else {
+		answer = "."
+	}
+
+	return answer
 }
--- Day10/day10_test.go
+++ Day10/day10_test.go
@@ -24,3 +24,51 @@ func Test_Part1WithActualData(t *testing.T) {
 		t.Error("Expected 14160, got", answer)
 	}
 }
+
+func Test_Part2WithExampleData(t *testing.T) {
+	inputLines, _ := inputfile.Read("testdata/example.txt")
+
+	expected := []string{
+		"##..##..##..##..##..##..##..##..##..##..",
+		"###...###...###...###...###...###...###.",
+		"####....####....####....####....####....",
+		"#####.....#####.....#####.....#####.....",
+		"######......######......######......####",
+		"#######.......#######.......#######....."}
+
+	answer := part2(inputLines)
+
+	if len(answer) != len(expected) {
+		t.Error("Expected equal lengths")
+	}
+
+	for i, v := range expected {
+		if answer[i] != v {
+			t.Error("String", i, "doesn't match")
+		}
+	}
+}
+
+func Test_Part2WithActualData(t *testing.T) {
+	inputLines, _ := inputfile.Read("testdata/actual.txt")
+
+	expected := []string{
+		"###....##.####.###..###..####.####..##..",
+		"#..#....#.#....#..#.#..#.#....#....#..#.",
+		"#..#....#.###..#..#.#..#.###..###..#....",
+		"###.....#.#....###..###..#....#....#....",
+		"#.#..#..#.#....#.#..#....#....#....#..#.",
+		"#..#..##..####.#..#.#....####.#.....##.."}
+
+	answer := part2(inputLines)
+
+	if len(answer) != len(expected) {
+		t.Error("Expected equal lengths")
+	}
+
+	for i, v := range expected {
+		if answer[i] != v {
+			t.Error("String", i, "doesn't match")
+		}
+	}
+}
