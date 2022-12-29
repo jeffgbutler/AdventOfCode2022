7c8c9dd23d13d1404312a42aad4b206893fdf8c5
Jeff Butler <jeffgbutler@gmail.com>
2022-12-29 07:23:04 -0500

    Day 21, part 2

--- Day21/day21.go
+++ Day21/day21.go
@@ -6,37 +6,99 @@ import (
 )
 
 type node interface {
-	value(nodes map[string]node) int
+	value(nodes map[string]node) (int, bool)
+	calculateMissingValue(nodes map[string]node, expectedValue int) int
 }
 
 type integerNode struct {
 	val int
 }
 
-func (n integerNode) value(_ map[string]node) int {
-	return n.val
+func (n integerNode) value(_ map[string]node) (int, bool) {
+	return n.val, true
+}
+
+func (n integerNode) calculateMissingValue(_ map[string]node, _ int) int {
+	// should never be called
+	panic("panic 2")
+}
+
+type humanNode struct{}
+
+func (n humanNode) value(_ map[string]node) (int, bool) {
+	return 0, false
+}
+
+func (n humanNode) calculateMissingValue(_ map[string]node, expectedValue int) int {
+	return expectedValue
 }
 
 type mathNode struct {
 	operation, left, right string
 }
 
-func (n mathNode) value(nodes map[string]node) int {
-	leftValue := nodes[n.left].value(nodes)
-	rightValue := nodes[n.right].value(nodes)
+func (n mathNode) value(nodes map[string]node) (int, bool) {
+	leftValue, okl := nodes[n.left].value(nodes)
+	rightValue, okr := nodes[n.right].value(nodes)
+
+	if !okl || !okr {
+		return 0, false
+	}
 
 	switch n.operation {
 	case "+":
-		return leftValue + rightValue
+		return leftValue + rightValue, true
 	case "-":
-		return leftValue - rightValue
+		return leftValue - rightValue, true
 	case "*":
-		return leftValue * rightValue
+		return leftValue * rightValue, true
 	case "/":
-		return leftValue / rightValue
+		return leftValue / rightValue, true
 	default:
-		return -1
+		return -1, false
 	}
+}
+
+func (n mathNode) calculateMissingValue(nodes map[string]node, expectedValue int) int {
+	leftValue, leftOk := nodes[n.left].value(nodes)
+	rightValue, rightOk := nodes[n.right].value(nodes)
+
+	if !leftOk {
+		switch n.operation {
+		case "+":
+			// l + r = e ---> l = e - r
+			return nodes[n.left].calculateMissingValue(nodes, expectedValue-rightValue)
+		case "-":
+			// l - r = e ---> l = e + r
+			return nodes[n.left].calculateMissingValue(nodes, expectedValue+rightValue)
+		case "*":
+			// l * r = e ---> l = e / r
+			return nodes[n.left].calculateMissingValue(nodes, expectedValue/rightValue)
+		case "/":
+			// l / r = e ---> l = e * r
+			return nodes[n.left].calculateMissingValue(nodes, expectedValue*rightValue)
+		}
+	}
+
+	if !rightOk {
+		switch n.operation {
+		case "+":
+			// l + r = e ---> r = e - l
+			return nodes[n.right].calculateMissingValue(nodes, expectedValue-leftValue)
+		case "-":
+			// l - r = e ---> l = e + r ---> l - e = r
+			return nodes[n.right].calculateMissingValue(nodes, leftValue-expectedValue)
+		case "*":
+			// l * r = e ---> r = e / l
+			return nodes[n.right].calculateMissingValue(nodes, expectedValue/leftValue)
+		case "/":
+			// l / r = e ---> l = e * r ---> l / e = r
+			return nodes[n.right].calculateMissingValue(nodes, leftValue/expectedValue)
+		}
+	}
+
+	// should never happen
+	panic("panic 1")
 }
 
 func part1(inputLines []string) int {
@@ -44,7 +106,18 @@ func part1(inputLines []string) int {
 
 	rootNode := nodes["root"]
 
-	return rootNode.value(nodes)
+	answer, _ := rootNode.value(nodes)
+	return answer
+}
+
+func part2(inputLines []string) int {
+	nodes := parsePart2(inputLines)
+
+	rootNode := nodes["root"]
+
+	answer := rootNode.calculateMissingValue(nodes, 0)
+
+	return answer
 }
 
 func parse(inputLines []string) map[string]node {
@@ -53,6 +126,25 @@ func parse(inputLines []string) map[string]node {
 	for _, inputLine := range inputLines {
 		name, node := parseInputLine(inputLine)
 		nodes[name] = node
+	}
+
+	return nodes
+}
+
+func parsePart2(inputLines []string) map[string]node {
+	nodes := map[string]node{}
+
+	for _, inputLine := range inputLines {
+		switch {
+		case strings.HasPrefix(inputLine, "humn"):
+			nodes["humn"] = humanNode{}
+		case strings.HasPrefix(inputLine, "root"):
+			name, node := parsePart2Root(inputLine)
+			nodes[name] = node
+		default:
+			name, node := parseInputLine(inputLine)
+			nodes[name] = node
+		}
 	}
 
 	return nodes
@@ -69,5 +161,19 @@ func parseInputLine(inputLine string) (string, node) {
 	} else {
 		//math node
 		return p1[0], mathNode{left: p2[0], operation: p2[1], right: p2[2]}
+	}
+}
+
+func parsePart2Root(inputLine string) (string, node) {
+	p1 := strings.Split(inputLine, ": ")
+	p2 := strings.Split(p1[1], " ")
+
+	if len(p2) == 1 {
+		// integer node
+		i, _ := strconv.Atoi(p2[0])
+		return p1[0], integerNode{val: i}
+	} else {
+		//math node
+		return p1[0], mathNode{left: p2[0], operation: "-", right: p2[2]}
 	}
 }
--- Day21/day21_test.go
+++ Day21/day21_test.go
@@ -24,3 +24,23 @@ func Test_Part1WithActualData(t *testing.T) {
 		t.Error("Expected 51928383302238, got", answer)
 	}
 }
+
+func Test_Part2WithExampleData(t *testing.T) {
+	inputLines, _ := inputfile.Read("testdata/example.txt")
+
+	answer := part2(inputLines)
+
+	if answer != 301 {
+		t.Error("Expected 301, got", answer)
+	}
+}
+
+func Test_Part2WithActualData(t *testing.T) {
+	inputLines, _ := inputfile.Read("testdata/actual.txt")
+
+	answer := part2(inputLines)
+
+	if answer != 3305669217840 {
+		t.Error("Expected 3305669217840, got", answer)
+	}
+}
